fc790034f1835c065c383b2e6dde4f1ce574afde
Jessica Anocibar <jessicayaws@yahoo.com>
2021-01-22 16:52:02 -0700

    pushing

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -24,7 +24,7 @@ export const Button = ({
             className={`btn ${checkButtonStyle} ${checkButtonSize}`}
             onClick={onClick}
             type={type}>
-                {children}
+                {children} 
             </button>
         </Link>
     )
