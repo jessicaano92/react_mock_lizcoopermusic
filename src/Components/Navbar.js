b3f127f53796ccdf9997b77fe6a999a2289ec7f5
Jessica Anocibar <jessicayaws@yahoo.com>
2021-01-23 11:12:27 -0700

    finished navbar adding new files

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,30 @@
 import React, { useState, } from "react";
 import { Link } from "react-router-dom";
 import './Navbar.css';
-import { Button } from './Button';
+
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+  // const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
-  const showButton = () => {
-      if (window.innerWidth <= 960) {
-          setButton(false)
-      } else {
-          setButton(true);
-      }
-  };
+  // const showButton = () => {
+  //     if (window.innerWidth <= 960) {
+  //         setButton(false)
+  //     } else {
+  //         setButton(true);
+  //     }
+  // };
 
-  window.addEventListener('resize', showButton);
+  // window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             Jessica Anocibar <i class="fab fa-angellist"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
@@ -55,7 +55,6 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>HOME</Button>}
         </div>
       </nav>
     </>
