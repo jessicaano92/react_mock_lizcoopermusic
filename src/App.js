330c3d015a6ef4a45575ac3083a89f42bee0f87d
Jessica Anocibar <jessicayaws@yahoo.com>
2021-01-23 12:05:46 -0700

    styled video, btns, and text

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './Components/Pages/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/' exact />
+        <Route path='/' exact component={Home} />
       </Switch>
     </Router>
   );
--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import { Button } from './Button'
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
-    return (
-        <div className='hero-container'>
-            <video src='https://youtu.be/uqidwQms2_0' autoPlay loop muted />
-            <p>Have a listen!</p>
-            <div className='hero-btns'>
-                <Button className='btns' buttonStyle='btn--outline'
-                buttonSize='btn--large'></Button>
-            </div>
-            
-        </div>
-    )
+  return (
+    <div className="hero-container">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/uqidwQms2_0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" autoPlay loop muted allowfullscreen></iframe>
+      <p>Music to my soul</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >ENTER</Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >WATCH ME<i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
